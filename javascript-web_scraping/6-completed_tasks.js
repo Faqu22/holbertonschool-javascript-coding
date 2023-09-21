b88a032dea055f3926770ab7110a76fa0932d07d
Faqu22 <facundoalvarez415@gmail.com>
2023-09-21 19:04:42 -0300

    Task 6 How many completed?

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+
+request.get(url, (err, response) => {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(response.body);
+  const completed = {};
+
+  data.forEach(task => {
+    const userId = task.userId;
+    if (task.completed) {
+      if (!completed[userId]) {
+        completed[userId] = 1;
+      } else {
+        completed[userId]++;
+      }
+    }
+  });
+  console.log(completed);
+});
